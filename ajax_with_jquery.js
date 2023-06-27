b614f3f5e1118886ba9d24e85ad2bb09f429740e
maserabri <briannatran45@gmail.com>
2023-06-26 20:40:04 -0700

    added file

---
+++ ajax_with_jquery.js
@@ -0,0 +1,22 @@
+/*The .ajax function accepts an object and returns a promise. Inside of the object you must use these keys like method, url etc.*/
+$.ajax({
+    // what HTTP verb?
+    method: "GET",
+    // where are we making a request to?
+    url: "https://omdbapi.com",
+    // what should we add to the query string?
+    data: {
+        // a key of t and a value of titanic which will look like ?t=titanic
+        t: 'titanic'
+    },
+    // this will add an HTTP request header of  'Accept': 'application/json'
+    dataType: "json"
+    // you can think of this ".then" like this: after we get the response, then what do we do? 
+}).then(function(response){
+    // let's see what the response is from the OMDB API!
+    console.log(response);
+}).catch(function(error){
+    // something went wrong :(
+    console.log(error);
+})
+
